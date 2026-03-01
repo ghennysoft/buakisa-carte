26ea574dd6965d564e397281b76dae1618bff450
ghennysoft <ghennysoft@gmail.com>
2026-03-01 09:50:49 +0100

    fix: good request 2

--- src/app/api/cards/[id]/route.ts
+++ src/app/api/cards/[id]/route.ts
@@ -1,13 +1,11 @@
-"use server";
-
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
 
   if (!id) {
     return NextResponse.json({ error: "ID manquant" }, { status: 400 });
